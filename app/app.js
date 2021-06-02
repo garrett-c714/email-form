d943c9b7b3a53e92ce7c2aaf9c9be7a93a985d57
Garrett Cox <gcox5714@gmail.com>
2021-06-02 14:51:35 -0400

    Starting Stuff 🤿

--- app/app.js
+++ app/app.js
@@ -4,8 +4,14 @@ const nodemailer = require('nodemailer');
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
     response.send('Nodemailer Test');
+});
+
+app.post('/email' , (request, response) => {
+
 });
 
 
