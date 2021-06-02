1df60c9fbae232445d0e6220dc889166ae240b20
Garrett Cox <gcox5714@gmail.com>
2021-06-02 16:28:44 -0400

    Started the Form 🎯

--- form/index.js
+++ form/index.js
@@ -1,2 +1,11 @@
 const form = document.querySelector('form');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
 
+
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    name.value = "";
+    email.value = "";
+});
